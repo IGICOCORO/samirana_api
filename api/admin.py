b90e8388ff4ba3db4f1df84e523365d0c61e9594
IGICOCORO <nduwimanaguymich@gmail.com>
2023-12-13 14:51:36 +0200

    disabled admin

--- api/admin.py
+++ api/admin.py
@@ -4,22 +4,23 @@ from .models import *
 # Register your models here.
 @admin.register(Enseignement)
 class EnseignementAdmin(admin.ModelAdmin):
-	list_display = "id", "name", "audio", "categorie", "date", "titre"
+	list_display = "id","audio", "categorie", "date", "titre"
 	list_filter = "id", "categorie", "date"
 	search_fields = "id", "name", "audio", "categorie", "date", "titre"
  
 @admin.register(Categorie)
 class CategorieAdmin(admin.ModelAdmin):
-	list_display = "id", "titre","categorie","author","text","date","slug"
-	list_filter =  "id","categorie","author","date"
-	search_fields =  "id", "titre","categorie","author","date",
+	list_display = "id", "name"
+	list_filter =  "id", "name"
+	search_fields =  "name",
 @admin.register(Annonce)
 class AnnonceAdmin(admin.ModelAdmin):
-	list_display = "id", "name","image"
-	list_filter = "id", "name", 
-	search_fields = "id", "name"
-@admin.register(Profile)
-class AnnonceAdmin(admin.ModelAdmin):
-	list_display = "id", "user","moi","image","birth_date"
-	list_filter = "id", "user","moi","image","birth_date"
-	search_fields = "id", "birth_date","user"
+	list_display =  "titre",
+	list_filter = "id",
+	search_fields = "id", "titre"
+ 
+# @admin.register(Profile)
+# class AnnonceAdmin(admin.ModelAdmin):
+# 	list_display = "id", "user","moi","image","birth_date"
+# 	list_filter = "id", "user","moi","image","birth_date"
+# 	search_fields = "id", "birth_date","user"
