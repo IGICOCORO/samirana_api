ac9569da59a01e0eae2b1872a8a7f0154d949b99
IGICOCORO <nduwimanaguymich@gmail.com>
2023-12-13 14:52:47 +0200

    disabled profile  on models

--- api/models.py
+++ api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Enseignement(models.Model):
     id = models.AutoField(primary_key=True)
-    audio = models.FileField(uploads_to="enseignements/")
-    categorie = models.ForeignKey(Category,on_delete=models.PROTECT)
-    date = models.DateField(verbose_name="Date de parution", default=timezone.now)
+    audio = models.FileField(max_length=100,upload_to="enseignements/")
+    categorie = models.ForeignKey("Categorie",on_delete=models.PROTECT)
+    date = models.DateField(verbose_name="Date de parution")
     titre = models.CharField(max_length=40)
     
     def __str__(self):
@@ -15,7 +15,6 @@ class Enseignement(models.Model):
 class Categorie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40, unique=True)
-    image = models.ImageField(null=True, blank=True, upload_to="categorie/")
     
     def __str__(self):
         return self.name
@@ -23,22 +22,19 @@ class Categorie(models.Model):
 class Annonce(models.Model):
     id = models.AutoField(primary_key=True)
     titre = models.CharField(unique=True, max_length=100)
-    categorie = models.ForeignKey('Categorie', null=True, on_delete=models.SET_NULL)
-    author = models.ForeignKey('Profile', null=True, on_delete=models.SET_NULL)
     text = models.TextField()
-    date = models.DateField(verbose_name="Date de parution", default=timezone.now)
-    slug = models.CharField(max_length=1000, blank=True, null=True)
+    date = models.DateField(verbose_name="Date de parution")
 
     def __str__(self):
         return self.titre
     
 
-class Profile(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    moi = models.TextField(max_length=500, blank=True, null=True)
-    image = models.ImageField(upload_to="profile/")
-    birth_date = models.DateField(null=True, blank=True)
+# class Profile(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     moi = models.TextField(max_length=500, blank=True, null=True)
+#     image = models.ImageField(upload_to="profile/")
+#     birth_date = models.DateField(null=True, blank=True)
 
-    def __str__(self):
-    	return f"{self.user.first_name} {self.user.last_name}"
+#     def __str__(self):
+#     	return f"{self.user.first_name} {self.user.last_name}"
